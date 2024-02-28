2c4235b71efef1174bcec30aacebb2b0112df136
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-02-28 15:00:10 +0300

    test

--- src/dashboard/Grafikler/LokasyonDagilimTable/LokasyonDagilimTable.jsx
+++ src/dashboard/Grafikler/LokasyonDagilimTable/LokasyonDagilimTable.jsx
@@ -52,6 +52,14 @@ const LokasyonDagilimTable = () => {
 
     return (
         <div style={{width: '100%', height: '100%'}}>
+            
+            <style>
+        {`
+          .ant-table-cell {
+            padding: 4px 16px !important;
+          }
+        `}
+      </style>
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <h3>Lokasyon Bazında İş talepleri / İş Emirleri Dağılımı</h3>
                 <Ayarlar chart={<Table
