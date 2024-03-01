bcd836842be17d14bacb1c83128385bf5f73fe4e
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-03-01 17:51:29 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <IsEmirlerininZamanDagilimiYearProvider>
           <PersonelIsGucuDateProvider>
             <ToplamHarcanaIsGucuDateProvider>
+              
               <IsEmriOzetTabloDateProvider>
                 <LokasyonDagilimDateProvider>
                   <BaseLayout />
